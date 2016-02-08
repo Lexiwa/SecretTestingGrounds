bf5e1ea8ee0182e472badff8ee1fd0eb8fc84fb3
0ctopusPrime <kul_bubbla@hotmail.com>
2016-02-08 13:45:16 +0100

    All the stuff I've done :DDDD
    Walla brursan.

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+private SpriteRenderer myRenderer;
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        myRenderer = GetComponent<SpriteRenderer>();
+        
+	}
+    Rigidbody2D rb;
+    // Update is called once per frame
+
+    void Update ()
+    {
+        speed = 12;
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 16;
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, speed));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector2(speed * -1, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector2(0, speed * -1));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector2(speed, 0));
+        }
+
+        myRenderer.sortingOrder = (int)(myRenderer.bounds.min.y * -10);
+    }
+}
